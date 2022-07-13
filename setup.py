0b8530d4b80146e5ec6f5f895059879116dbbf98
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2022-07-13 12:49:16 +0300

    added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Infant-Crying-Detection',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='liubomyr.ivanitskyi',
+    author_email='',
+    description=''
+)
